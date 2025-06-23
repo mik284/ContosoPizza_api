753622f85c308832ca524627107ba358a79ed61b
mik284 <mikeykamau222@gmail.com>
2025-06-23 15:06:30 +0300

    Configure Swagger documentation for ContosoPizza API

--- Program.cs
+++ Program.cs
@@ -5,7 +5,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "ContosoPizza",
+            Version = "v1",
+            Description = "A simple example ASP.NET Core Web API",
+            Contact = new Microsoft.OpenApi.Models.OpenApiContact
+            {
+                Name = "Mike Kamau",
+                Email = "mikeykamau222@gmail.com",
+                Url = new Uri("https://github.com/mik284/ContosoPizza.git")
+            },
+            License = new Microsoft.OpenApi.Models.OpenApiLicense
+            {
+                Name = "Use this license",
+                Url = new Uri("https://github.com/mik284/ContosoPizza/blob/main/LICENSE")
+            }
+        });
+    }
+);
 
 var app = builder.Build();
 
